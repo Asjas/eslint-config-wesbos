75cf6e9b00a632aeb206519fa2b00ce0350d969a
A-J Roos <asjas@hey.com>
2021-02-08 20:50:43 +0200

    add initial react typescript config

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const eslintReact = require('./src/eslint-react');
+const eslintReactTS = require('./src/eslint-react-ts');
 const prettierConfig = require('./src/prettier');
 
 module.exports = {
   eslintReact,
+  eslintReactTS,
   prettierConfig,
 };
---
+++ src/eslint-react-ts.js
@@ -0,0 +1,32 @@
+module.exports = {
+  extends: [
+    'plugin:@typescript-eslint/recommended', // Uses rules from `@typescript-eslint/eslint-plugin`,
+    'airbnb-typescript',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    './base.js',
+  ],
+  // then add some extra good stuff for Typescript
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  // Then we add our own custom typescript rules
+  rules: {
+    // This allows us to use async function on addEventListener(). Discussion: https://twitter.com/wesbos/status/1337074242161172486
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: false,
+      },
+
+    ],
+    'react/jsx-filename-extension': [
+      1,
+      {
+        extensions: ['.ts', '.tsx'],
+      },
+    ],
+  },
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+};

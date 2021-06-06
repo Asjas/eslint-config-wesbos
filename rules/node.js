26fb879d040448a1c869440299735abbad571cc9
A-J Roos <asjas@hey.com>
2021-06-06 12:32:58 +0200

    update node rules to allow esm

--- rules/node.js
+++ rules/node.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: ["node"],
   extends: ["plugin:node/recommended"],
-  rules: {},
+  settings: {
+    node: {
+      allowModules: ["electron"],
+      resolvePaths: [__dirname],
+      tryExtensions: [".js", ".ts", ".json", ".node"],
+    },
+  },
+  rules: {
+    "node/no-unsupported-features/es-syntax": 0,
+  },
 };
